56be6eb801d367968591d35f9d13930c97931f9c
C <ray20000309@icloud.com>
2022-04-28 21:22:35 +0800

    Add other animal click event

---
+++ Assets/Scripts/Newborn Scene/giraffeController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class giraffeController : MonoBehaviour
+{
+    public Animator[] animators;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        foreach (Animator ani in animators)
+        {
+            ani.SetBool("Trigger", true);
+        }
+    }
+    public void backidle()
+    {
+        foreach (Animator ani in animators)
+        {
+            ani.SetBool("Trigger", false);
+        }
+    }
+}

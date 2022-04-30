c24679473e4671b3c91eb96d2ee3283aafeab944
C <ray20000309@icloud.com>
2022-04-30 22:15:25 +0800

    whale scene2 init
    whale scene2 init
    adjust elephant2 newborn other animals position

---
+++ Assets/Scripts/Whale Scene/Carema_Controller.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carema_Controller : MonoBehaviour
+{
+    public bool isUseMoveOnScreenEdge = true;
+    private float moveSpeed = 5f;
+    private int ScreenEdgeSize = 50;
+
+    private bool MoveUp;
+    private bool MoveDown;
+    private bool MoveRight;
+    private bool MoveLeft;
+    private Rect RigthRect;
+    private Rect UpRect;
+    private Rect DownRect;
+    private Rect LeftRect;
+    private Vector3 dir = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isUseMoveOnScreenEdge)
+        {
+            UpRect = new Rect(1f, Screen.height - ScreenEdgeSize, Screen.width, ScreenEdgeSize);
+            DownRect = new Rect(1f, 1f, Screen.width, ScreenEdgeSize);
+
+            LeftRect = new Rect(1f, 1f, ScreenEdgeSize, Screen.height);
+            RigthRect = new Rect(Screen.width - ScreenEdgeSize, 1f, ScreenEdgeSize, Screen.height);
+
+
+            MoveUp = (UpRect.Contains(Input.mousePosition));
+            MoveDown = (DownRect.Contains(Input.mousePosition));
+
+            MoveLeft = (LeftRect.Contains(Input.mousePosition));
+            MoveRight = (RigthRect.Contains(Input.mousePosition));
+
+            dir.y = MoveUp ? 1 : MoveDown ? -1 : 0;
+            dir.x = MoveLeft ? -1 : MoveRight ? 1 : 0;
+
+            transform.position = Vector3.Lerp(transform.position, transform.position + dir * moveSpeed, Time.deltaTime);
+        }
+    }
+}

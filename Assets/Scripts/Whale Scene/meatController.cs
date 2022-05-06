95de9705897e388e4716a93cb83bd271d1d09127
C <ray20000309@icloud.com>
2022-05-06 17:19:18 +0800

    whale update
    update collider
    add scene 3,4 animation

--- Assets/Scripts/Whale Scene/meatController.cs
+++ Assets/Scripts/Whale Scene/meatController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeatController : MonoBehaviour
+public class meatController : MonoBehaviour
 {
 	private Vector3 selfScenePosition;
 	private bool hasAttracted = false;

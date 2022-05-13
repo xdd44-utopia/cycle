b2995b745b66055df2e1a5662356ae9980c6cb91
XDD44 <50290408+xdd44-utopia@users.noreply.github.com>
2022-05-13 13:21:20 +0800

    revise arches

--- Assets/Scripts/Migration Scene/Dust.cs
+++ Assets/Scripts/Migration Scene/Dust.cs
@@ -6,6 +6,7 @@ public class Dust : MonoBehaviour
 	public SpriteRenderer sprite;
 	public bool isDarken;
 	public SpriteRenderer[] bkgs;
+	public Animator anim;
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -16,10 +17,16 @@ public class Dust : MonoBehaviour
 	void Update()
 	{
 		sprite.color = new Color(1, 1, 1, (30 - 10 - this.transform.position.x + elephant.transform.position.x) / 30);
-		if (this.transform.position.x - elephant.transform.position.x + 10 > 0) {
+		if (this.transform.position.x - elephant.transform.position.x + 10 > 0 && this.transform.position.x - elephant.transform.position.x + 10 < 40) {
 			foreach (SpriteRenderer bkg in bkgs) {
 				bkg.color = isDarken ? sprite.color : new Color(1, 1, 1, 1 - sprite.color.a);
 			}
+		}
+		if (elephant.transform.position.x - this.transform.position.x > 0 && elephant.transform.position.x - this.transform.position.x < 10) {
+			anim.SetBool("Dark", isDarken);
+		}
+		else if (elephant.transform.position.x - this.transform.position.x < 0 && elephant.transform.position.x - this.transform.position.x > -10) {
+			anim.SetBool("Dark", !isDarken);
 		}
 	}
 }

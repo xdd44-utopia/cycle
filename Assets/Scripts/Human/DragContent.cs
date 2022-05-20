3e0d4246106ebc93f586db7a28295c730c467e1d
C <ray20000309@icloud.com>
2022-05-20 22:27:19 +0800

    Human Subway update

--- Assets/Scripts/Human/DragContent.cs
+++ Assets/Scripts/Human/DragContent.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonController : MonoBehaviour
+public class DragContent : MonoBehaviour
 {
+    private Transform content;
+    
     // Start is called before the first frame update
     void Start()
     {
+        content = this.gameObject.transform.GetChild(0);
         
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+    private void OnMouseDrag()
     {
         
     }
